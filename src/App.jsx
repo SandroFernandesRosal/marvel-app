4cd80374095d8b2f5774a6d26585747683f49643
Sandro Fernandes <sandrofernandesrosal@gail.com>
2023-03-26 18:29:27 -0300

    refatorando

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 
 import { ThemeProvider } from "styled-components";
 import { GloballStyles } from "./Themes/GlobalStyles";
-import CartProvider from "./Context/Cart";
 import { AppRoutes } from "./Routes/Approutes";
 import usePersistedState from "./Hooks/usePersistedState";
 import dark from "./themes/dark";
 import light from "./themes/light";
-import { useState } from "react";
+
 
 
 
 function App() {
-  const [ theme, setTheme ] = usePersistedState('theme', dark);
-
-  
+  const [ theme, setTheme ] = usePersistedState('theme',dark);
 
   const handleChangeTheme = () => {
     
@@ -21,15 +18,13 @@ function App() {
     
   }
 
-
-  
   return (
     <ThemeProvider theme={theme}>
-        <CartProvider>
+        
             <GloballStyles/>
               
             <AppRoutes handleChangeTheme={handleChangeTheme} theme={theme} />
-      </CartProvider>
+      
     </ThemeProvider>
   )
 }
--- src/Components/Cards/CardCharacter.jsx
+++ src/Components/Cards/CardCharacter.jsx
@@ -10,14 +10,10 @@ export const CardCharacter = () => {
 
   const [characters, setCharacters ] = useState([]);
   const [ cardDescription, setCardDescription ] = useState(false);
-  const [ search, setSearch ] = useState('');
- 
-
+  
   const handleDescription = () => {
     cardDescription === false ? setCardDescription(true) : setCardDescription(false);
   }
-
-  
 
 
   useEffect(() => {
@@ -34,39 +30,39 @@ export const CardCharacter = () => {
     <>
      
      <Search setCharacters={setCharacters}/>
-    
-    
+
     <Container>
         
-        {characters.map(character => {
+    {characters.map(character => {
               return( 
-                <>
-                 <CardItens key={character.id}  onClick={handleDescription}>
-              <img 
-               src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-               alt={`Foto do ${character.name}`} />
-               <span>{character.name}</span>
-               <button className="car" type="button"> <BsFillCartCheckFill /></button>
+<>
+            <CardItens key={character.id}  onClick={handleDescription}>
+                      <img 
+                      src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+                      alt={`Foto do ${character.name}`} />
+                      <span>{character.name}</span>
+                      <button className="car" type="button"> <BsFillCartCheckFill /></button>
                
-              </CardItens>
-              {cardDescription && 
-              <ContainerDescription >
+            </CardItens>
+
+    {cardDescription && 
+            <ContainerDescription >
               
-              <CardDescriptions  >
-                  <img src={`${character.thumbnail.path}.${character.thumbnail.extension}`} />
-                  <div className="descriptions">
-                    <button type="button" onClick={handleDescription}>X</button>
-                    <h2>{character.name}</h2>
-                    <p>{character.description}</p>
-                  </div>
-              </CardDescriptions>
+                  <CardDescriptions  >
+                      <img src={`${character.thumbnail.path}.${character.thumbnail.extension}`} />
+                      <div className="descriptions">
+                          <button type="button" onClick={handleDescription}>X</button>
+                          <h2>{character.name}</h2>
+                          <p>{character.description}</p>
+                      </div>
+                  </CardDescriptions>
               
-            </ContainerDescription>
+              </ContainerDescription>
               }
               
-                 </>
+</>
               )
-            })}
+         })}
   
     </Container>
     </>
